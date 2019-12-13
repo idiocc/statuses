b2e541422d0dfd1f7cbc58bd5d29186988680db2
Anton <anton@adc.sh>
2019-12-13 23:03:56 +0300

    add tests

--- test/spec/default.js
+++ test/spec/default.js
@@ -1,12 +1,25 @@
-import { equal } from '@zoroaster/assert'
+import { equal, ok } from '@zoroaster/assert'
 import Context from '../context'
-import { STATUS_CODES } from '../../src'
+import { STATUS_CODES, redirect, retry, empty } from '../../src'
 
 /** @type {Object.<string, (c: Context)>} */
 const T = {
   context: Context,
   'STATUS_CODES contains codes'() {
     equal(STATUS_CODES[100], 'Continue')
+  },
+  redirect() {
+    equal(redirect[200], undefined)
+    ok(redirect[301])
+  },
+  retry() {
+    equal(retry[501], undefined)
+    ok(retry[503])
+  },
+  empty() {
+    equal(empty[200], undefined)
+    ok(empty[204])
+    ok(empty[304])
   },
 }
 
